ad88062b0ebc31c3c9728a67ec0f4c49d8448aa9
Jasper Yu <007seadog@gmail.com>
2017-10-31 01:54:18 +0800

    Basically, rewrited the program, fixed #2

--- run.sh
+++ run.sh
@@ -1,76 +1,74 @@
 #!/bin/bash
 
-function login(){
-	read -p "Username: " username
-	read -s -p "Password: " password
-	echo ''
-	rm cookie &>/dev/null
-	token=`curl -c cookie -s -o /dev/null 'https://www.plurk.com/login' \
-		| grep -oh 'login_token" value=".*"\ \/>\ <input\ type="hidden"\ name="logintoken"\ value="1"\ \/>' \
-		| sed -e 's/login_token"\ value="//g' -e 's/"\ \/>\ <input\ type="hidden"\ name="logintoken"\ value="1"\ \/>//g'`
-	curl -L -c cookie -s -o /dev/null 'https://www.plurk.com/Users/login' --data "nick_name=$username&password=$password&login_token=$token&logintoken=1"
+trap "exit 1" TERM
+export TOP_PID=$$
+
+function stderr(){
+	# For user messages.
+	echo -e $1 >&2
 }
 
 function username2id(){
-	curl -s "https://www.plurk.com/$1" | grep -oh 'showFriends?user_id=.*&page=all' | sed -e 's/showFriends?user_id=//g' -e 's/&page=all//g'
+	# Convert the username (offically they called nickname) to userid.
+	# The format for userid is \d+
+	id=`curl -s "https://www.plurk.com/$1" | grep -oh 'showFriends?user_id=.*&page=all' | sed -e 's/showFriends?user_id=//g' -e 's/&page=all//g'`
+	[ -z "$id" ] && stderr 'User not exist, Exiting...' && kill -s TERM $TOP_PID
+	echo $id
 }
 
 function getuserdata(){
-	echo "-----------$1-----------"
-	curl -b cookie -s 'https://www.plurk.com/Users/getUserData' --data "page_uid=$1" | jq -r '"Fans: " + (.num_of_fans | tostring) + "\n" + "Friend: " + (.num_of_friends | tostring) + "\n" + "Mutual Friend: " + (.num_of_mutual_friends | tostring)'
-	curl -b cookie -s 'https://www.plurk.com/Friends/getMyFriendsCompletion' -X POST | jq -r '.["'$1'"] | "Username: " + (.nick_name | tostring) + "\n" + "Nickname: " +  (.display_name | tostring) + "\n" + "Realname: " +  (.full_name | tostring) '
+	echo "-----------$2-----------"
+	curl -s 'https://www.plurk.com/Users/getUserData' --data "page_uid=$1" | jq -r '"Fans: " + (.num_of_fans | tostring) + "\n" + "Friend: " + (.num_of_friends | tostring)'
 }
 
 function get_friends_by_offset(){
+	# Print the friend list (user ID) of a specific user ID to stdout.
 	userid=$1
+	stderr "- User id: $userid"
+	friendcount=`get_friends_count $userid`
 	offset=0
 	while true
 	do
-			res=`curl -b cookie -s 'https://www.plurk.com/Friends/getFriendsByOffset' --data "offset=$offset&user_id=$userid"`
-			[ "$res" == '[]' ] && return 0
-			echo $res | sed 's/"date_of_birth":\ new Date(".\{1,30\}"),//g' | jq '.[].uid'
-			offset=$(($offset + 10))
-	done
-}
-
-function get_mutual_friends_by_offset(){
-	userid=$1
-	offset=0
-	while true
-	do
-		res=`curl -b cookie -s 'https://www.plurk.com/Friends/getMutualFriendsByOffset' --data "offset=$offset&user_id=$userid"`
+		stderr "- Fetch $offset of $friendcount"
+		res=`curl -s 'https://www.plurk.com/Friends/getFriendsByOffset' --data "offset=$offset&user_id=$userid"`
 		[ "$res" == '[]' ] && return 0
-		echo $res | sed 's/"date_of_birth":\ new Date(".\{1,30\}"),//g' | jq '.[].uid'
+		echo $res | sed 's/"date_of_birth":\ new Date(".\{1,30\}"),//g' | jq '.[] | [(.uid | tostring), .nick_name] | join(",")'
 		offset=$(($offset + 10))
 	done
 }
 
+function get_friends_count(){
+	# Get friends count, but doesn't check the username, so be sure check the username existence before use.
+	userid=$1
+	echo `curl -s "https://www.plurk.com/Friends/showFriends?user_id=$userid&page=" | grep -oP "'friends',.+?,\s\K\d+"`
+}
+
 function and_list(){
-	#A跟B都有看到
+	# List $1 and list $2 both have the plurk.
+	# A跟B都有看到
 	awk 'FNR==NR{ array[$0]; next} {if ( $1 in array ) print $1 ;next}' $1 $2
 }
 
 function subtract_list(){
-	#A有看到B沒看到
+	# List $1 have the item, but list $2 doesn't.
+	# A有看到B沒看到
 	awk 'FNR==NR{ array[$0]; next} {if ( $1 in array ) next; print $1}' $2 $1
 }
 
 function clean_up(){
-	rm tmp_final
-	rm cookie
+	rm -f tmp
+	rm -f tmp_final
 }
 
-
-login
-echo "Logging...."
+clean_up
 c=1
 total=`wc -l rule`
 while read line
 do
-	echo "$c of $total:"
+	stderr "$c of $total:"
 	rule=${line:0:1}
 	username=${line:1:${#line}-1}
-	echo "Fetch friends list of $username" && get_friends_by_offset `username2id $username` > tmp
+	stderr "Fetch friends list of $username" && get_friends_by_offset `username2id $username` > tmp
 	[ $c -eq 1 ] && mv tmp tmp_final
 	[ $c -gt 1 ] && [ "$rule" == "+" ] && echo "$username can see the plurk" && and_list tmp_final tmp > tmp_tmp
 	[ $c -gt 1 ] && [ "$rule" == "-" ] && echo "$username cannot see the plurk" && subtract_list tmp_final tmp > tmp_tmp
@@ -78,10 +76,12 @@ do
 	c=$(($c + 1))
 done < rule
 
-echo -e "\n\nList possible person:\n"
+stderr "\n\nList possible person:\n"
 while read line
 do
-	getuserdata $line
+	userid=`echo $line | awk -F ',' '{print $1}'`
+	username=`echo $line | awk -F ',' '{print $2}'`
+	getuserdata $userid $username
 done < tmp_final
 
 clean_up
